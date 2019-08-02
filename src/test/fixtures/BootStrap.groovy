af4e94ee001ac495ab5859c1e294baf40fd940a0
Peter Legen <peter.legen@zlavadna.sk>
2019-08-02 15:33:32 +0200

    added with transaction block to bootstrap
    Don’t know how is this working without flush or transaction. In grails 4 i had to add transaction block but it should be required also in grail 3…

--- src/test/fixtures/BootStrap.groovy
+++ src/test/fixtures/BootStrap.groovy
@@ -10,37 +10,39 @@ class BootStrap {
     PasswordService credentialMatcher
 
     def init = { servletContext ->
-        def adminRole = ShiroRole.findByName("Administrator")
-        if (!adminRole) {
-            adminRole = new ShiroRole(name: "Administrator")
-            def adminUser = new ShiroUser(username: "admin", passwordHash: credentialMatcher.encryptPassword("admin"))
-            adminUser.addToRoles(adminRole)
-            adminUser.save()
-            assert credentialMatcher.passwordsMatch('admin', adminUser.passwordHash)
+        ShiroRole.withTransaction {
+            def adminRole = ShiroRole.findByName("Administrator")
+            if (!adminRole) {
+                adminRole = new ShiroRole(name: "Administrator")
+                def adminUser = new ShiroUser(username: "admin", passwordHash: credentialMatcher.encryptPassword("admin"))
+                adminUser.addToRoles(adminRole)
+                adminUser.save()
+                assert credentialMatcher.passwordsMatch('admin', adminUser.passwordHash)
 
-            def userRole = new ShiroRole(name: "User")
-            def normalUser = new ShiroUser(username: "dilbert", passwordHash: credentialMatcher.encryptPassword("password"))
-            normalUser.addToRoles(userRole)
-            normalUser.addToPermissions("book:show,index,read")
-            normalUser.save()
-            assert credentialMatcher.passwordsMatch('password', normalUser.passwordHash)
+                def userRole = new ShiroRole(name: "User")
+                def normalUser = new ShiroUser(username: "dilbert", passwordHash: credentialMatcher.encryptPassword("password"))
+                normalUser.addToRoles(userRole)
+                normalUser.addToPermissions("book:show,index,read")
+                normalUser.save()
+                assert credentialMatcher.passwordsMatch('password', normalUser.passwordHash)
 
-            // Users for the TestController.
-            def testRole = new ShiroRole(name: "test")
-            testRole.addToPermissions("book:*")
-            
-            def testUser1 = new ShiroUser(username: "test1", passwordHash: credentialMatcher.encryptPassword("test1"))
-            testUser1.addToRoles(testRole)
-            testUser1.addToRoles(userRole)
-            testUser1.addToPermissions("custom:read,write")
+                // Users for the TestController.
+                def testRole = new ShiroRole(name: "test")
+                testRole.addToPermissions("book:*")
 
-            testUser1.save()
-            assert credentialMatcher.passwordsMatch('test1', testUser1.passwordHash)
+                def testUser1 = new ShiroUser(username: "test1", passwordHash: credentialMatcher.encryptPassword("test1"))
+                testUser1.addToRoles(testRole)
+                testUser1.addToRoles(userRole)
+                testUser1.addToPermissions("custom:read,write")
 
-            // Some initial books that we can test against.
-            new Book(name: "Colossus", author: "Niall Ferguson").save()
-            new Book(name: "Misery", author: "Stephen King").save()
-            new Book(name: "Guns, Germs, and Steel", author: "Jared Diamond").save()
+                testUser1.save()
+                assert credentialMatcher.passwordsMatch('test1', testUser1.passwordHash)
+
+                // Some initial books that we can test against.
+                new Book(name: "Colossus", author: "Niall Ferguson").save()
+                new Book(name: "Misery", author: "Stephen King").save()
+                new Book(name: "Guns, Germs, and Steel", author: "Jared Diamond").save()
+            }
         }
     }
 

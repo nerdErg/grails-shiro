e80e448be65ad02e4dfd58064dc323320d03d6d7
pmcneil <peter@nerderg.com>
2020-05-26 14:38:37 +1000

    try a different method to resolve auth exception.

--- src/main/groovy/org/apache/shiro/grails/ShiroGrailsExceptionResolver.groovy
+++ src/main/groovy/org/apache/shiro/grails/ShiroGrailsExceptionResolver.groovy
@@ -1,15 +1,21 @@
 package org.apache.shiro.grails
 
-import grails.artefact.controller.support.ResponseRedirector
 import grails.core.GrailsApplication
 import grails.core.support.GrailsApplicationAware
+import grails.web.api.WebAttributes
+import grails.web.mapping.LinkGenerator
+import grails.web.mapping.ResponseRedirector
+import grails.web.mapping.mvc.RedirectEventListener
 import org.apache.shiro.authz.AuthorizationException
 import org.apache.shiro.authz.UnauthenticatedException
 import org.codehaus.groovy.runtime.InvokerInvocationException
 import org.grails.exceptions.ExceptionUtils
 import org.grails.web.servlet.mvc.exceptions.GrailsMVCException
+import org.grails.web.util.GrailsApplicationAttributes
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver
+import org.springframework.web.servlet.support.RequestDataValueProcessor
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -19,21 +25,48 @@ import java.lang.reflect.InvocationTargetException
  * Date: 5/07/19
  *
  */
-class ShiroGrailsExceptionResolver  extends SimpleMappingExceptionResolver implements GrailsApplicationAware, ResponseRedirector {
+class ShiroGrailsExceptionResolver  extends SimpleMappingExceptionResolver implements GrailsApplicationAware {
 
     protected GrailsApplication grailsApplication
+    private LinkGenerator linkGenerator
+    private RequestDataValueProcessor requestDataValueProcessor
+    private Collection<RedirectEventListener> redirectListeners
+
+    @Autowired(required=false)
+    void setRedirectListeners(Collection<RedirectEventListener> redirectListeners) {
+        this.redirectListeners = redirectListeners
+    }
+
+    @Autowired(required = false)
+    void setRequestDataValueProcessor(RequestDataValueProcessor requestDataValueProcessor) {
+        this.requestDataValueProcessor = requestDataValueProcessor
+    }
+
+    @Autowired
+    void setGrailsLinkGenerator(LinkGenerator linkGenerator) {
+        this.linkGenerator = linkGenerator
+    }
+
+    LinkGenerator getGrailsLinkGenerator() {
+        if(this.linkGenerator == null) {
+            this.linkGenerator = webRequest.getApplicationContext().getBean(LinkGenerator)
+        }
+        return this.linkGenerator
+    }
 
     @Override
     ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
                                   Object handler, Exception ex) {
-
+        if(request.getAttribute(GrailsApplicationAttributes.REDIRECT_ISSUED)){
+            println("************ Already redirected!")
+        }
         Exception authEx = findAuthException(ex)
         if (authEx) {
             if(authEx instanceof UnauthenticatedException) {
-                loginRedirect(grailsApplication, request)
+                loginRedirect(request, response)
             }
             if(authEx instanceof AuthorizationException) {
-                unauthorizedRedirect(grailsApplication, request)
+                unauthorizedRedirect(request, response)
             }
             return new ModelAndView()
         }
@@ -71,34 +104,56 @@ class ShiroGrailsExceptionResolver  extends SimpleMappingExceptionResolver imple
         return ExceptionUtils.getRootCause(ex)
     }
 
-    private static void loginRedirect(GrailsApplication grailsApplication, HttpServletRequest request) {
+    private void loginRedirect(HttpServletRequest request, HttpServletResponse response) {
 
         String targetUri = getTargetUri(request)
         String redirectUri = grailsApplication.config.getProperty('security.shiro.login.uri')
 
         if (redirectUri) {
-            redirectUri(uri: redirectUri)
+            redirect([uri: redirectUri],request, response)
         } else {
-            redirectUri(
-                    controller: (grailsApplication.config.getProperty('security.shiro.login.controller') ?: "auth"),
+            redirect(
+                    [controller: (grailsApplication.config.getProperty('security.shiro.login.controller') ?: "auth"),
                     action: (grailsApplication.config.getProperty('security.shiro.login.action') ?: "login"),
-                    params: [targetUri: targetUri.toString()]
+                    params: [targetUri: targetUri?.toString()]],request, response
             )
         }
     }
 
-    static void unauthorizedRedirect(GrailsApplication grailsApplication, HttpServletRequest request) {
+    private void unauthorizedRedirect(HttpServletRequest request, HttpServletResponse response) {
         String targetUri = getTargetUri(request)
         String redirectUri = grailsApplication.config.getProperty('security.shiro.unauthorized.uri')
         if (redirectUri) {
-            redirectUri(uri: redirectUri)
+            redirect([uri: redirectUri],request, response)
         } else {
-            redirectUri(
-                    controller: (grailsApplication.config.getProperty('security.shiro.unauthorized.controller') ?: "auth"),
+            redirect(
+                    [controller: (grailsApplication.config.getProperty('security.shiro.unauthorized.controller') ?: "auth"),
                     action: (grailsApplication.config.getProperty('security.shiro.unauthorized.action') ?: "unauthorized"),
-                    params: [targetUri: targetUri.toString()]
+                    params: [targetUri: targetUri.toString()]], request, response
             )
         }
+    }
+
+    /**
+     * Redirects for the given arguments.
+     *
+     * @param argMap The arguments
+     * @return null
+     */
+    void redirect(Map argMap, HttpServletRequest request, HttpServletResponse response) {
+
+        if (argMap.isEmpty()) {
+            throw new IllegalArgumentException("Invalid arguments for method 'redirect': $argMap")
+        }
+
+        getGrailsLinkGenerator()
+
+        ResponseRedirector redirector = new ResponseRedirector(getGrailsLinkGenerator())
+        redirector.setRedirectListeners redirectListeners
+        redirector.setRequestDataValueProcessor requestDataValueProcessor
+        redirector.setUseJessionId false
+
+        redirector.redirect request, response, argMap
     }
 
     private static String getTargetUri(HttpServletRequest request) {
